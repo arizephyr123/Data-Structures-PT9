776ed9e984bd48dc2b71047df2a02f9dca63247b
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-07-23 00:21:40 -0700

    queue array fns completed, tests pass

--- queue/queue.py
+++ queue/queue.py
@@ -2,17 +2,58 @@
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
 
-1. Implement the Queue class using an array as the underlying storage structure.
-   Make sure the Queue tests pass.
-2. Re-implement the Queue class, this time using the linked list implementation
-   as the underlying storage structure.
-   Make sure the Queue tests pass.
+Make sure the Queue tests pass.
+
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# 1. Implement the Queue class using an array as the underlying storage structure.
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+    
+    def __len__(self):
+        return len(self.storage)
+
+    def enqueue(self, value):
+        self.storage.insert(0, value)
+
+    def dequeue(self):
+        if len(self.storage) == 0:
+            return None
+        return self.storage.pop()
+
+'''
+q = Queue()
+q.enqueue(100)
+print(q.storage)
+q.enqueue(101)
+print(q.storage)
+q.enqueue(105)
+print(q.storage)
+q.dequeue()
+print(q.storage)
+print(len(q), 2)
+q.dequeue()
+print(q.storage)
+print(len(q), 1)
+q.dequeue()
+print(q.storage)
+print(len(q), 0)
+q.dequeue()
+print(q.storage)
+print(len(q), 0)
+'''
+
+'''
+# 2. Re-implement the Queue class, this time using the linked list implementation
+   as the underlying storage structure.
+
 from singly_linked_list import LinkedList 
 class Queue:
     def __init__(self):
@@ -31,3 +72,5 @@ class Queue:
         return self.storage.remove_head()
 
         # self.storage.length -= 1
+
+'''
